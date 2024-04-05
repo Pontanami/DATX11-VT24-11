4d6b6620a9bb9bc7172c97185438de37b81e024a
Omar Zahran <omar.erik.zahran@gmail.com>
2024-04-05 20:20:38 +0200

    added some comments

--- src/transpiler/Environment.java
+++ src/transpiler/Environment.java
@@ -4,14 +4,14 @@ package transpiler;
 public class Environment {
     public static final String RUNTIME_PACKAGE = "transpiler.runtime";
 
-    // Create an identifier that can't be defined in the source language to void name conflicts
+    // Create an identifier that can't be defined in the source language to avoid name conflicts
     public static String reservedId(String source) {
         return "_" + source;
     }
 
-    // Return a valid java identifier, if the source is a java keyword that isn't a key word in the project language,
-    // then the returned string should be an equivalent
+    // If the source is a java keyword that isn't a keyword in the project language,
+    // then the returned string should be an converted into a valid java identifier
     public static String escapeJavaKeyword(String source) {
-        throw new RuntimeException("Not implemented");
+        throw new RuntimeException("Not implemented"); //TODO: implementera om det behövs
     }
 }
