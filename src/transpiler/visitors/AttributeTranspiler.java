70b2811995ed55a2d2445c109665339cfde27ade
dpmram <danmalmg@net.chalmers.se>
2024-04-17 17:57:10 +0200

    Refactored transpile logic into separate method

--- src/transpiler/visitors/AttributeTranspiler.java
+++ src/transpiler/visitors/AttributeTranspiler.java
@@ -1,37 +1,39 @@
 package transpiler.visitors;
 
 import grammar.gen.ConfluxParser;
-import grammar.gen.ConfluxParserBaseVisitor;
-
 
 public class AttributeTranspiler extends DefaultTranspiler {
+
     @Override
     public String visitAttributesBlock(ConfluxParser.AttributesBlockContext ctx) {
-            StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
-            // Visit each attribute in the attributes block
-            for (ConfluxParser.DeclarationContext declaration : ctx.declaration()) {
-                //if the 'var' keyword is not present, add private final to the attribute
-                if (declaration.VAR() == null) {
-                    result.append("private final ");
-                }
-
-                //Attributes without assignments
-                if (declaration.variableDeclaration() != null) {
-                    result.append(visit(declaration.variableDeclaration()));
-                }
-
-                //Attributes with assignments
-                else {
-                    result.append(visit(declaration.type()));
-                    result.append(" ");
-
-                    result.append(visit(declaration.assignment()));
-                }
-                result.append(";");
-                result.append("\n");
-            }
-            return result.toString();
+        // Visit each attribute in the attributes block and transpile
+        for (ConfluxParser.DeclarationContext declaration : ctx.declaration()) {
+            result.append(transpileAttribute(declaration)).append(";\n");
         }
+        return result.toString();
+    }
+
+    public String transpileAttribute(ConfluxParser.DeclarationContext declaration) {
+        StringBuilder result = new StringBuilder();
+
+        //if the 'var' keyword is not present, add private final to the attribute
+        if (declaration.VAR() == null) {
+            result.append("private final ");
+        }
+
+        //Attributes without assignments
+        if (declaration.variableDeclaration() != null) {
+            result.append(visit(declaration.variableDeclaration()));
+        }
+
+        //Attributes with assignments
+        else {
+            result.append(visit(declaration.type())).append(" ")
+                    .append(visit(declaration.assignment()));
+        }
+        return result.toString();
+    }
 }
 
