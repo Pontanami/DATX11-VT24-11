8424c839c047c92808013f70394c79a878440f3d
Pontanami <pontusen@student.chalmers.se>
2024-04-19 08:54:30 +0200

    Create StartVisitor.java

---
+++ src/transpiler/visitors/StartVisitor.java
@@ -0,0 +1,27 @@
+package transpiler.visitors;
+
+import grammar.gen.ConfluxParser;
+import grammar.gen.ConfluxParserBaseVisitor;
+import transpiler.tasks.TaskQueue;
+
+public class StartVisitor extends ConfluxParserBaseVisitor<Void> {
+    private TaskQueue taskQueue;
+    public StartVisitor(TaskQueue taskQ) {
+     this.taskQueue = taskQ;
+    }
+
+    @Override
+    public Void visitProgram(ConfluxParser.ProgramContext ctx) {
+        if (ctx.typeDeclaration() == null) {
+            return null;
+        }
+        //Need to generate a class since only the interface block is mandatory
+        if (ctx.typeDeclaration().getChildCount() > 1){
+            ctx.typeDeclaration().accept(new ClassTranspiler(taskQueue, new StatementTranspiler(new ObserverTranspiler(taskQueue))));
+        }
+        else{
+            ctx.accept(new InterfaceTranspiler(taskQueue));
+        }
+        return null;
+    }
+}
