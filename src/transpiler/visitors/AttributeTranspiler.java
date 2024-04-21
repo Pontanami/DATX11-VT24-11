9e0a5a6413150bb0d155c8874dca2ceeb347f874
dpmram <danmalmg@net.chalmers.se>
2024-04-21 23:08:09 +0200

    Added getter generator

--- src/transpiler/visitors/AttributeTranspiler.java
+++ src/transpiler/visitors/AttributeTranspiler.java
@@ -1,6 +1,7 @@
 package transpiler.visitors;
 
 import grammar.gen.ConfluxParser;
+import java_builder.MethodBuilder;
 
 
 public class AttributeTranspiler extends DefaultTranspiler {
@@ -9,6 +10,14 @@ public class AttributeTranspiler extends DefaultTranspiler {
     @Override
     public String visitAttributeDeclaration(ConfluxParser.AttributeDeclarationContext ctx) {
 
+        if (ctx.AS() != null) {
+            return generateGetter(ctx);
+        } else {
+            return generateAttribute(ctx);
+        }
+    }
+
+    private String generateAttribute(ConfluxParser.AttributeDeclarationContext ctx) {
         StringBuilder result = new StringBuilder();
 
         result.append("private ");
@@ -16,18 +25,31 @@ public class AttributeTranspiler extends DefaultTranspiler {
         if (ctx.declaration().VAR() == null) {
             result.append("final ");
         }
-
         result.append(visit(ctx.declaration().type()));
+        result.append(" ").append(visitDeclarationPart(ctx.declaration().declarationPart(0)));
+        result.append(";");
+        return result.toString();
+    }
 
-        if (!ctx.declaration().declarationPart().isEmpty()) {
-            for (ConfluxParser.DeclarationPartContext part : ctx.declaration().declarationPart()) {
-                result.append(" ").append(visitDeclarationPart(part));
-            }
+    private String generateGetter(ConfluxParser.AttributeDeclarationContext ctx) {
+
+        String returnType = visit(ctx.declaration().type());
+        String attributeName = ctx.declaration().declarationPart(0).Identifier().getText();
+        String methodName = "get" + attributeName; //TODO: implement correct methodName
+
+
+        MethodBuilder methodBuilder = new MethodBuilder();
+        methodBuilder.addModifier("private");
+        if (ctx.declaration().VAR() == null) {
+            methodBuilder.addModifier("final");
         }
+        methodBuilder.setReturnType(returnType);
+        methodBuilder.setIdentifier(methodName);
+        methodBuilder.addStatement("return " + attributeName + ";");
 
+        return methodBuilder.toCode();
 
-        return result.toString();    }
-
+    }
 
     public String visitDeclarationPart(ConfluxParser.DeclarationPartContext ctx) {
         StringBuilder result = new StringBuilder();
@@ -36,7 +58,6 @@ public class AttributeTranspiler extends DefaultTranspiler {
         if (ctx.ASSIGN() != null) {
             result.append(" = ").append(visit(ctx.expression()));
         }
-        result.append(";");
 
         return result.toString();
     }
